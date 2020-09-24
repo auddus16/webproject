31185a819f4e085204596ab7c05d1ddbee3d3279
auddus16 <auddus16@naver.com>
2020-09-24 19:14:49 +0900

    ssiiiiiballl

--- inyro/dbconn.php
+++ inyro/dbconn.php
@@ -2,7 +2,7 @@
 $mysql_host="172.16.24.191";
 $mysql_user="auddus16";
 $mysql_password="1234";
-$mysql_db="inyro";
+$mysql_db="inyroweb";
 
 $conn=mysqli_connect($mysql_host, $mysql_user, $mysql_password, $mysql_db);
 
