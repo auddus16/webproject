7479d555ac2f1d9e2ee96a68a51d7f109498521d
sally5131 <yeyeah5131@naver.com>
2020-11-17 22:23:14 +0900

    ff

--- inyro/pyj_modify_update_post.php
+++ inyro/pyj_modify_update_post.php
@@ -20,29 +20,13 @@ $mb_id=$_SESSION['ss_mb_id'];
 // $mb_id = trim($_POST['mb_id']);
 $post_title2 = trim($_POST['post_title']);
 $post_category_id		= trim($_POST['post_category_id']);
-$post_datatime				= date('Y-m-d H:i:s', time());
 $post_text2				= trim($_POST['post_text']);
 $post_file	= trim($_POST['post_file']);
-$post_image	= trim($_POST['post_image']);
-$post_comment_count		= trim($_POST['post_comment_count']);
-$post_hit		= trim($_POST['post_hit']);
-$post_like				= trim($_POST['post_like']);
-$post_modify_datetime	= date('Y-m-d H:i:s', time());
-$post_delete_datetime	= date('Y-m-d H:i:s', time());
-$post_del				= trim($_POST['post_del']);
-$post_ip				= trim($_POST['post_ip']);
+
 echo "<script>alert('$post_title2')</script>";
 echo "<script>alert('$post_category_id')</script>";
-echo "<script>alert('$post_datatime')</script>";
 echo "<script>alert('$post_text2')</script>";
 echo "<script>alert('$post_file')</script>";
-echo "<script>alert('$post_image')</script>";
-echo "<script>alert('$post_comment_count')</script>";
-echo "<script>alert('$post_hit')</script>";
-echo "<script>alert('$post_like')</script>";
-echo "<script>alert('$post_delete_datetime')</script>";
-echo "<script>alert('$post_del')</script>";
-echo "<script>alert('$post_ip')</script>";
 
 
 if (!$mb_id) {
@@ -83,17 +67,9 @@ if (!$post_text2) {
 $sql = "UPDATE post SET
 post_title = '$post_title2',
 post_category_id		= '$post_category_id',
-post_datatime				= '$post_datatime',
 post_text				= '$post_text2',
 post_file	= '$post_file',
-post_image	= '$post_image',
-post_comment_count		= '$post_comment_count',
-post_hit		= '$post_hit',
-post_like				= '$post_like',
-post_modify_datetime	= '$post_modify_datetime',
-post_delete_datetime	= '$post_delete_datetime',
-post_del				= '$post_del',
-post_ip				= '$post_ip'
+post_modify_datetime	= current_timestamp()
 WHERE mb_id='$mb_id'";
 
 $result = mysqli_query($conn, $sql);
@@ -101,17 +77,5 @@ $result = mysqli_query($conn, $sql);
 if($result == false){echo mysqli_connect_error($conn);}
 echo "해당 글이 수정되었습니다.";
 
-echo "<script>alert('$post_title2')</script>";
-echo "<script>alert('$post_category_id')</script>";
-echo "<script>alert('$post_datatime')</script>";
-echo "<script>alert('$post_text2')</script>";
-echo "<script>alert('$post_file')</script>";
-echo "<script>alert('$post_image')</script>";
-echo "<script>alert('$post_comment_count')</script>";
-echo "<script>alert('$post_hit')</script>";
-echo "<script>alert('$post_like')</script>";
-echo "<script>alert('$post_delete_datetime')</script>";
-echo "<script>alert('$post_del')</script>";
-echo "<script>alert('$post_ip')</script>";
 mysqli_close($conn); // 데이터베이스 접속 종료
 ?>
